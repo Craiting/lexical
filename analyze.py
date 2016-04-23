49ef4d60de234de3246cc97bc4f9d00d460dcb93
Craig Tingey <tingeycraig@gmail.com>
2016-04-23 14:37:43 -0600

    parsed the file

---
+++ analyze.py
@@ -0,0 +1,49 @@
+import re
+myfile = raw_input()
+
+fin = open(myfile, 'r')
+
+lines = fin.readlines()
+
+def breakup_line(line):
+    words = line.split()
+    for i in range(len(words)):
+        if words[i][0] in ("'",'"') and words[i][-1] in ("'",'"'): # don't break strings
+            pass
+        else: # break up further based on punctuation
+            t = re.findall(r"[\w]+|[^\s\w]", words[i])
+            if len(t) > 1:
+                words.remove(words[i])
+                words[i:i] = t
+    return words
+            
+def get_strings(words):
+    new_words = []
+    adding = False
+    tmpstring = ''
+    skip = False
+    for w in words:
+        if w in ("'",'"'):
+            adding = True
+        elif w[-1] in ('"',"'"):
+            adding = False
+            tmpstring += w + ' '
+            new_words.append(tmpstring)
+            tmpstring = ''
+            skip = True
+        if adding:
+            tmpstring += w + ' '
+        else:
+            if skip:
+                skip = False
+            else:
+                new_words.append(w)
+    return new_words
+            
+            
+            
+
+for line in lines:
+    tokens = breakup_line(line)
+    final = get_strings(tokens)
+    print final
---
+++ test.py
@@ -0,0 +1,9 @@
+a = [1,2,3,4]
+b = 'Hey'
+c = "hello"
+d = 'this should not breakup'
+
+print b,c
+
+def thing(word):
+    return word + 'added words'
